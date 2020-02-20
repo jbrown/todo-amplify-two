9d2255223b7677a3492b526df2ce0c10521480d0
Justin Brown <justinbrown.work@gmail.com>
2020-02-20 09:15:32 -0600

    Add test for signing out.

--- cypress/integration/authenticator_spec.js
+++ cypress/integration/authenticator_spec.js
@@ -14,6 +14,21 @@ describe("Authenticator:", function() {
     // Step 3: Make an assertion (Check for sign-out text)
     cy.get(selectors.signOutButton).contains("Sign Out");
   });
+
+  it("allows a user to signout", () => {
+    // Step 2: Take an action (Sign in)
+    cy.get(selectors.usernameInput).type("testuser");
+    cy.get(selectors.signInPasswordInput).type("password");
+    cy.get(selectors.signInSignInButton)
+      .contains("Sign In")
+      .click();
+
+    // Step 3: Make an assertion (Check for sign-out text)
+    cy.get(selectors.signOutButton)
+      .contains("Sign Out")
+      .click();
+    cy.get(selectors.signInSignInButton).should("be.visible");
+  });
 });
 
 export const selectors = {
